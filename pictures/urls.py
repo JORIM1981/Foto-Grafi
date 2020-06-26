250e6f98c9be729d1e99ea5f79bd1e4c2bf1a3b5
JORIM1981 <okongo.midumbi.opondo@gmail.com>
2020-06-26 15:00:03 +0300

    add changes to index.html

--- pictures/urls.py
+++ pictures/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-app_name = 'pictures'
+
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
