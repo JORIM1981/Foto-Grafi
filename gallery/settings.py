c64ab4d9ca0d8a8bd5f55a81e9fbcf1f1291b217
JORIM1981 <okongo.midumbi.opondo@gmail.com>
2020-06-26 13:34:02 +0300

    add media folder and all the gallery pics

--- gallery/settings.py
+++ gallery/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -124,3 +125,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
